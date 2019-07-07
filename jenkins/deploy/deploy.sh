5c8bd5daa1d104b593b6e4dfb6e62ac7bfb378c2
jenkins <jenkins@localhost.localdomain>
2019-07-07 09:27:38 -0400

    bug

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,7 +3,7 @@ echo app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/jenkins/prod /tmp/.auth jenkins@192.168.0.57:/tmp/.auth
-scp -i /home/jenkins/prod ./jenkins/deploy/publish.sh jenkins@192.168.0.57:/tmp/publish.sh
+scp -i /opt/prod /tmp/.auth jenkins@192.168.0.57:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh jenkins@192.168.0.57:/tmp/publish.sh
 
-ssh -i /home/jenkins/prod jenkins@192.168.0.57 "/tmp/publish.sh"
+ssh -i /opt/prod jenkins@192.168.0.57 "/tmp/publish.sh"
