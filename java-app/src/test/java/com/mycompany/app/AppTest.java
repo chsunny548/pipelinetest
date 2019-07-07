b5920d7796c3db70301c33c254f9b2c61b1198bd
jenkins <jenkins@localhost.localdomain>
2019-07-07 09:56:58 -0400

    Hello world

--- java-app/src/test/java/com/mycompany/app/AppTest.java
+++ java-app/src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World! This is a maven application \n My name is Sunny" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
