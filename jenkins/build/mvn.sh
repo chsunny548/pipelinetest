6211935f4c45d0733d5c7fecbc95891aaa629c45
jenkins <jenkins@localhost.localdomain>
2019-07-07 09:14:52 -0400

    bug fix

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,5 @@
 echo "********************************"
 echo "*******Building JAR*************"
 echo "********************************"
-
-docker container run --rm -v /home/jenkins/hello/pipeline/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
+WORKSPACE=/home/jenkins/hello/jenkins_home/workspace/pipeline-maven-proj
+docker container run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
