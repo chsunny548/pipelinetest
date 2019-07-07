3baebd6a1dc9434deda8f8d8b28b56485132e2f3
jenkins <jenkins@localhost.localdomain>
2019-07-07 07:55:56 -0400

    Buildfix

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,4 @@ echo "********************************"
 echo "*******Building JAR*************"
 echo "********************************"
 
-docker container run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
+docker container run --rm -v /home/jenkins/hello/pipeline/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3-alpine "$@"
